2ba34c48998e837fe07c7d2ed06b912fa9a9e5f6
Numpkens <numpkins1228@gmail.com>
2026-02-26 14:26:46 +0400

    clean up file structure

--- parser-core/src/lib.rs
+++ parser-core/src/lib.rs
@@ -1,48 +1,8 @@
-use std::fs;
-use nom::{
-    bytes::complete::take,
-    number::complete::le_u32,
-    IResult,
-};
 
-#[derive(Debug)]
-struct EvtcLog {
-    magic: String,
-    version: String,
-    agent_count: u32,
-}
+pub mod model;   
+pub mod parser;  
 
-fn parse_evtc(input: &[u8]) -> IResult<&[u8], EvtcLog> {
-    let (input, magic_bytes) = take(4usize)(input)?;
-    let (input, version_bytes) = take(8usize)(input)?;
-    let (input, agent_count) = le_u32(input)?;
 
-    Ok((input, EvtcLog {
-        magic: String::from_utf8_lossy(magic_bytes).into_owned(),
-        version: String::from_utf8_lossy(version_bytes).into_owned(),
-        agent_count,
-    }))
-}
-
-fn main() {
-    
-    let file_path = "test.evtc"; 
-
-    match fs::read(file_path) {
-        Ok(bytes) => {
-            println!("Read {} bytes from file.", bytes.len());
-
-      
-            match parse_evtc(&bytes) {
-                Ok((_remaining, log)) => {
-                    println!("--- Parsed Successfully ---");
-                    println!("{:#?}", log);
-                }
-                Err(e) => println!("Failed to parse EVTC structure: {:?}", e),
-            }
-        }
-        Err(e) => {
-            println!("Could not open file '{}': {}", file_path, e);
-        }
-    }
+pub extern "C" fn parse_log(ptr: *const u8, len: usize) {
+    //implement the Go-FFI call
 }
