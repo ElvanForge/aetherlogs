eaf29cc226effe168f572125ba3a7472a2c82c7a
Numpkens <numpkins1228@gmail.com>
2026-03-26 14:25:34 +0400

    add struct for json string in main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+type Agent struct {
+	Address       uint64 `json:"address"`
+	Name          string `json:"name"`
+	Profession    uint32 `json:"profession"`
+	IsElite       uint32 `json:"is_elite"`
+	Toughness     uint16 `json:"toughness"`
+	Concentration uint16 `json:"concentration"`
+	Healing       uint16 `json:"healing"`
+	Condition     uint16 `json:"condition"`
+}
+
+type ParserResult struct {
+	BossID uint16 `json:"boss_id"`
+	Agents []Agent `json:"agents"`
+}
+
+
 func main() {
 	fmt.Println("AetherLogs: Orchestrator Active")
 }
